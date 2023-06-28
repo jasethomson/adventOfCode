0fe2c6a989cb2dfa6001f09d19c2041200ae7ffd
Jase <jasethomson31@gmail.com>
2023-06-28 15:24:07 -0700

    completed part 1

--- main/2022/2022-01/index.ts
+++ main/2022/2022-01/index.ts
@@ -1,22 +1,32 @@
 import path from "path";
 import {
   readFilePath,
-  splitFileDataByNewLine,
   splitFileDataByCarriageReturnAndNewLine,
 } from "../../utils";
 
 const rootFilePath = "../../static/2022/2022-01/";
-const mode: "sample" | "" = "sample";
-const filePath: string =
-  mode === "sample"
-    ? path.join(__dirname, `${rootFilePath}sample-input.txt`)
-    : path.join(__dirname, `${rootFilePath}input.txt`);
+const filePath: string = path.join(
+  __dirname,
+  `${rootFilePath}sample-input.txt`
+);
 
 (async () => {
   const fileData = await readFilePath(filePath);
-  const fileDataByLine =
-    filePath.indexOf("sample") !== -1
-      ? splitFileDataByNewLine(fileData)
-      : splitFileDataByCarriageReturnAndNewLine(fileData);
-  console.log(fileDataByLine.length);
+  const fileDataByLine = splitFileDataByCarriageReturnAndNewLine(fileData);
+  let position = 1;
+  const elves = [{ position, calories: 0 }];
+  let topCalorieElf: null | { position: number; calories: number } = null;
+  fileDataByLine.forEach((line, count) => {
+    const currentElf = elves[elves.length - 1];
+    if (!topCalorieElf) topCalorieElf = currentElf;
+    if (!line && fileDataByLine[count + 1]) {
+      elves.push({ position: ++position, calories: 0 });
+    } else if (line) {
+      currentElf.calories += parseInt(line);
+    }
+    if (!line && topCalorieElf?.calories < currentElf.calories) {
+      topCalorieElf = currentElf;
+    }
+  });
+  console.log(topCalorieElf); // Part 1 - 71780 calories position 35
 })();
