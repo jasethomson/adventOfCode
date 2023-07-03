ff715a9ab1716f7466eab21607c0a763e9117a17
Jase <jasethomson31@gmail.com>
2023-07-03 11:46:37 -0700

    completed day 6

--- main/2022/day-06/index.ts
+++ main/2022/day-06/index.ts
@@ -6,12 +6,14 @@ const rootFilePath = `../../static/${date}/`;
 
 const filePath: string = path.join(__dirname, `${rootFilePath}input.txt`);
 
+const uniqueCharacterLength = 14;
+
 const findPacketStart = (
   charactersArr: string[],
   currentIndex: number = 0,
   uniqueCharacters: string[] = []
 ): number => {
-  if (uniqueCharacters.length === 4) return currentIndex;
+  if (uniqueCharacters.length === uniqueCharacterLength) return currentIndex;
   const characterIndex = uniqueCharacters.indexOf(charactersArr[currentIndex]);
   uniqueCharacters.push(charactersArr[currentIndex++]);
   if (characterIndex !== -1) {
@@ -25,5 +27,5 @@ const findPacketStart = (
   const fileDataByLine = splitFileDataByNewLine(fileData);
   const charactersArr = fileDataByLine[0].split("");
   const packetStart = findPacketStart(charactersArr);
-  console.log("packetStart", packetStart);
+  console.log("packetStart", packetStart); // part 1: 1262 - part 2: 3444
 })();
