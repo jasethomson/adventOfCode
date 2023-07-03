91d8df5c078152498adefa4f076ef72cdc05c2e8
Jase <jasethomson31@gmail.com>
2023-07-03 11:37:03 -0700

    Completed day 6 part 1

--- main/2022/day-06/index.ts
+++ main/2022/day-06/index.ts
@@ -6,8 +6,24 @@ const rootFilePath = `../../static/${date}/`;
 
 const filePath: string = path.join(__dirname, `${rootFilePath}input.txt`);
 
+const findPacketStart = (
+  charactersArr: string[],
+  currentIndex: number = 0,
+  uniqueCharacters: string[] = []
+): number => {
+  if (uniqueCharacters.length === 4) return currentIndex;
+  const characterIndex = uniqueCharacters.indexOf(charactersArr[currentIndex]);
+  uniqueCharacters.push(charactersArr[currentIndex++]);
+  if (characterIndex !== -1) {
+    uniqueCharacters = uniqueCharacters.slice(characterIndex + 1);
+  }
+  return findPacketStart(charactersArr, currentIndex, uniqueCharacters);
+};
+
 (async () => {
   const fileData = await readFilePath(filePath);
   const fileDataByLine = splitFileDataByNewLine(fileData);
-  console.log(fileDataByLine);
+  const charactersArr = fileDataByLine[0].split("");
+  const packetStart = findPacketStart(charactersArr);
+  console.log("packetStart", packetStart);
 })();
