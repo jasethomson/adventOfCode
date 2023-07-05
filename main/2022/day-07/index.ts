8bbe595e5a4d0271ebd2650e6ecb830d05be71e3
Jase <jasethomson31@gmail.com>
2023-07-04 20:40:25 -0700

    started day 7

--- main/2022/day-07/index.ts
+++ main/2022/day-07/index.ts
@@ -4,10 +4,47 @@ import { readFilePath, splitFileDataByNewLine } from "../../utils";
 const date = __dirname.split("dist")[1].replace(/\\/g, "/");
 const rootFilePath = `../../static/${date}/`;
 
-const filePath: string = path.join(__dirname, `${rootFilePath}input.txt`);
+const filePath: string = path.join(
+  __dirname,
+  `${rootFilePath}sample-input.txt`
+);
 
 (async () => {
   const fileData = await readFilePath(filePath);
   const fileDataByLine = splitFileDataByNewLine(fileData);
   console.log(fileDataByLine);
+  let path = "";
+  const pathObj: { [key: string]: {} } = {};
+  fileDataByLine.forEach((cl, count) => {
+    console.log("CURRENT PATH", path);
+    if (cl.startsWith("$")) {
+      const clArr = cl.split(" ");
+      console.log("cmd found", clArr);
+      if (clArr[1] === "cd") {
+        if (clArr[2] === "..") {
+          console.log("move up dir", path);
+          if (path) {
+            const pathSplitArr = path.split("/");
+            path = pathSplitArr.slice(0, pathSplitArr.length - 1).join("/");
+          }
+        } else if (clArr[2] === "/") {
+          console.log("reset path dir", path);
+          path = "";
+          console.log("reset path dir new path", path);
+        } else {
+          console.log("move down dir", path);
+          if (path) {
+            path += `/${clArr[2]}`;
+          } else {
+            path = clArr[2];
+            if (!pathObj[clArr[2]]) pathObj[clArr[2]] = {};
+          }
+          console.log("move down dir new path", path);
+        }
+      }
+    } else {
+      console.log("data found", cl);
+    }
+    console.log(pathObj);
+  });
 })();
