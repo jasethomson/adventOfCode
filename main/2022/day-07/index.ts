34e9e10514a200564c9ba0a44faedff8cb0e9002
Jase <jasethomson31@gmail.com>
2023-07-17 20:44:48 -0700

    Completed day 7 adventOfCode

--- main/2022/day-07/index.ts
+++ main/2022/day-07/index.ts
@@ -4,47 +4,126 @@ import { readFilePath, splitFileDataByNewLine } from "../../utils";
 const date = __dirname.split("dist")[1].replace(/\\/g, "/");
 const rootFilePath = `../../static/${date}/`;
 
-const filePath: string = path.join(
-  __dirname,
-  `${rootFilePath}sample-input.txt`
-);
+const filePath: string = path.join(__dirname, `${rootFilePath}input.txt`);
+
+interface NodeStructure {
+  name: string;
+  size: number;
+  children: NodeStructure[] | null;
+  parent: NodeStructure | null;
+}
+
+interface DirSizeObj {
+  name: string;
+  size: number;
+}
+
+const createNode = (
+  name: string,
+  size: number,
+  children: NodeStructure[] | null,
+  parent: NodeStructure | null
+): NodeStructure => ({
+  name,
+  size,
+  children,
+  parent,
+});
+
+const processInstructions = (lines: string[]): NodeStructure => {
+  const rootNode = createNode("/", 0, [], null);
+  let currentDir = rootNode;
+  lines.forEach((line) => {
+    if (!line) return;
+    const lineSplit = line.split(" ");
+    if (line[0] === "$") {
+      const [_, __, arg] = lineSplit;
+      switch (arg) {
+        case "/":
+          currentDir = rootNode;
+          break;
+        case "..":
+          currentDir = currentDir.parent || rootNode;
+          break;
+        default:
+          if (!currentDir.children) return;
+          const subDir = currentDir.children.find(
+            (child) => child.children && child.name === arg
+          );
+          if (subDir) currentDir = subDir;
+      }
+    } else if (line.startsWith("dir")) {
+      const [_, arg] = lineSplit;
+      if (!currentDir.children) return;
+      currentDir.children.push(createNode(arg, 0, [], currentDir));
+    } else {
+      const [size, name] = lineSplit;
+      if (!currentDir.children) return;
+      currentDir.children.push(
+        createNode(name, parseInt(size), null, currentDir)
+      );
+    }
+  });
+
+  return rootNode;
+};
+
+const processNodes = (
+  node: NodeStructure,
+  accumulator: number = 0,
+  dirSizeMap: DirSizeObj[] = []
+): {
+  size: number;
+  total: number;
+  dirSizeMap: DirSizeObj[];
+} => {
+  if (!node.children) {
+    return {
+      size: node.size,
+      total: accumulator,
+      dirSizeMap,
+    };
+  }
+
+  let size = 0;
+  node.children.forEach((child) => {
+    const result = processNodes(child, accumulator, dirSizeMap);
+    size += result.size;
+    accumulator = result.total;
+    dirSizeMap = result.dirSizeMap;
+  });
+  dirSizeMap.push({ name: node.name, size });
+  if (size <= 100000) {
+    accumulator += size;
+  }
+  return { size, total: accumulator, dirSizeMap };
+};
 
 (async () => {
   const fileData = await readFilePath(filePath);
   const fileDataByLine = splitFileDataByNewLine(fileData);
-  console.log(fileDataByLine);
-  let path = "";
-  const pathObj: { [key: string]: {} } = {};
-  fileDataByLine.forEach((cl, count) => {
-    console.log("CURRENT PATH", path);
-    if (cl.startsWith("$")) {
-      const clArr = cl.split(" ");
-      console.log("cmd found", clArr);
-      if (clArr[1] === "cd") {
-        if (clArr[2] === "..") {
-          console.log("move up dir", path);
-          if (path) {
-            const pathSplitArr = path.split("/");
-            path = pathSplitArr.slice(0, pathSplitArr.length - 1).join("/");
-          }
-        } else if (clArr[2] === "/") {
-          console.log("reset path dir", path);
-          path = "";
-          console.log("reset path dir new path", path);
-        } else {
-          console.log("move down dir", path);
-          if (path) {
-            path += `/${clArr[2]}`;
-          } else {
-            path = clArr[2];
-            if (!pathObj[clArr[2]]) pathObj[clArr[2]] = {};
-          }
-          console.log("move down dir new path", path);
-        }
-      }
-    } else {
-      console.log("data found", cl);
+
+  // part 1
+  const dirStructure = processInstructions(fileDataByLine);
+  const { size, total, dirSizeMap } = processNodes(dirStructure);
+
+  console.log("total size to free up", total);
+
+  // part2
+  const totalDiskSpace = 70000000;
+  const minDiskSpace = 30000000;
+  const unUsedSpace = totalDiskSpace - size;
+  const neededSpace = minDiskSpace - unUsedSpace;
+
+  let removableDir: DirSizeObj | null = null;
+  dirSizeMap.forEach((dir) => {
+    if (!removableDir) {
+      if (dir.size >= neededSpace) removableDir = dir;
+      return;
     }
-    console.log(pathObj);
+    if (dir.size >= neededSpace && dir.size < removableDir.size) {
+      removableDir = dir;
+    }
   });
+  console.log("smallest removable dir", removableDir);
 })();
